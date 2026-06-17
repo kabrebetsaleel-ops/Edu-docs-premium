bd30385c3da7a04eea076dba5162b0a2e294fb34
kabrebetsaleel-ops <kabrebetsaleel@gmail.com>
2026-06-17 19:26:41 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'edudocs-v1';
+const ASSETS = [
+  '/index.html',
+  '/auth.html',
+  '/mes-docs.html',
+  '/admin.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(console.error)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
